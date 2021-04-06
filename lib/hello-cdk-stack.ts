d464a625ad05ad64aea6832ebcd72b8a45eb8ec8
Mitsuhito Fujita <mitsuhitofujita@gmail.com>
2021-04-06 22:54:46 +0900

    :tada: #1 add lambda function.

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import {NodejsFunction} from '@aws-cdk/aws-lambda-nodejs';
+import { Runtime } from '@aws-cdk/aws-lambda';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const helloFunc = new NodejsFunction(this, 'Hello', {
+      entry: 'lambda/hello/index.ts',
+      runtime: Runtime.NODEJS_12_X,
+    });
   }
 }
